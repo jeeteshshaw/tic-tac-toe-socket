241709ee209890418d0050a52e17ed9b4ddc8cc1
jeetesh kumar shaw <jeeteshshaw@gmail.com>
2024-10-02 13:01:49 +0530

    port changed

--- index.js
+++ index.js
@@ -345,7 +345,7 @@ const express = require('express');
 const app = express();
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 5000;
 
 var io = socket(server, {
     pingInterval: 10000,
